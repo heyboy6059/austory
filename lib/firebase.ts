328300430189b30ac6cd15bdadc3e2ff9f41a2b3
Daniel Park <dpark@littles.co>
2021-10-20 21:54:02 +1000

    initial app environment setup and base strctures

---
+++ lib/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZM4GcEwLya_cjnIHbn2qJth7gnw-U0QU",
+  authDomain: "austory-danpark.firebaseapp.com",
+  projectId: "austory-danpark",
+  storageBucket: "austory-danpark.appspot.com",
+  messagingSenderId: "937500945011",
+  appId: "1:937500945011:web:f83bf20ccf46139ebfd806",
+  measurementId: "G-FXZVL0QDVQ",
+}
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
