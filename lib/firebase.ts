640b3799d9a9d0fb359354bb41f6f3d46c8564f8
Daniel Park <dpark@littles.co>
2021-10-21 00:22:57 +1000

    base login ui

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,7 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+import "firebase/compat/storage"
 
 const firebaseConfig = {
   apiKey: "AIzaSyDZM4GcEwLya_cjnIHbn2qJth7gnw-U0QU",
@@ -14,9 +14,11 @@ const firebaseConfig = {
 }
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig)
+  firebase.initializeApp(firebaseConfig)
 }
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-export const storage = firebase.storage();
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+export const storage = firebase.storage()
+
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,13 @@ import styles from "../styles/Home.module.css"
 import Link from "next/link"
 
 import Loader from "../components/Loader"
+import toast from "react-hot-toast"
 
 export default function Home() {
   return (
     <div>
-      {/* <Link
-        prefetch={false}
-        href={{
-          pathname: "/[username]",
-          query: {
-            username: "jeffd23",
-          },
-        }}
-      >
-        <a>Jeff's profile</a>
-      </Link> */}
-      <Loader show />
+      {/* <Loader show /> */}
+      <button onClick={() => toast.success("hello toast!")}>Toast Me</button>
     </div>
   )
 }
