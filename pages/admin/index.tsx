43dc4f8fee733163eeac082c67e28fb36380cb4f
Daniel Park <dpark@littles.co>
2021-10-27 23:11:44 +1000

    base post, auth check, metatags, 404 error page components

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,3 +1,9 @@
-export default function AdminPostsPage({}) {
-  return <main></main>
+import AuthCheck from "../../components/AuthCheck"
+
+export default function AdminPostsPage(props) {
+  return (
+    <main>
+      <AuthCheck></AuthCheck>
+    </main>
+  )
 }
