e044a6957bbd303c8b4d92fb99d986dfab191130
Daniel Park <dpark@littles.co>
2021-11-09 00:13:53 +1000

    added inkrau.vercel.app in next config domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost:3000", "lh3.googleusercontent.com"],
+    domains: [
+      "localhost:3000",
+      "lh3.googleusercontent.com",
+      "inkrau.vercel.app",
+    ],
   },
 }
