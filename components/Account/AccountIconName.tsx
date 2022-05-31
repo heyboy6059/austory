e4ace714f28ae73efc3ed6153b215a229cf44d07
Daniel Park <dpark@littles.co>
2022-05-31 22:30:41 +1000

    added tooltips

--- components/Account/AccountIconName.tsx
+++ components/Account/AccountIconName.tsx
@@ -11,6 +11,7 @@ import AirIcon from '@mui/icons-material/Air'
 import SkateboardingIcon from '@mui/icons-material/Skateboarding'
 import { COLOURS } from '../../common/constants'
 import { Role } from '../../typing/interfaces'
+import Tooltip from '@mui/material/Tooltip'
 
 interface Props {
   username: string
@@ -22,71 +23,87 @@ const AccountIconName: FC<Props> = ({ username, role }) => {
       {username === '인크라우' ? (
         <StarsIcon style={{ fontSize: '16px', color: 'darksalmon' }} />
       ) : role === Role.PARTNER ? (
-        <LocalPoliceIcon
-          style={{
-            fontSize: '17px',
-            color: COLOURS.BRIGHT_GREEN,
-            marginTop: '0.5px'
-          }}
-        />
+        <Tooltip title="파트너 에디터" placement="bottom" arrow>
+          <LocalPoliceIcon
+            style={{
+              fontSize: '17px',
+              color: COLOURS.BRIGHT_GREEN,
+              marginTop: '0.5px'
+            }}
+          />
+        </Tooltip>
       ) : role === Role.WORKER ? (
-        <WorkIcon
-          style={{
-            fontSize: '17px',
-            color: COLOURS.PRIMARY_BLUE,
-            marginTop: '0.5px'
-          }}
-        />
+        <Tooltip title="직장인" placement="bottom" arrow>
+          <WorkIcon
+            style={{
+              fontSize: '17px',
+              color: COLOURS.PRIMARY_BLUE,
+              marginTop: '0.5px'
+            }}
+          />
+        </Tooltip>
       ) : role === Role.STUDENT ? (
-        <MenuBookIcon
-          style={{
-            fontSize: '17px',
-            color: COLOURS.PRIMARY_BLUE,
-            marginTop: '0.5px'
-          }}
-        />
+        <Tooltip title="학생" placement="bottom" arrow>
+          <MenuBookIcon
+            style={{
+              fontSize: '17px',
+              color: COLOURS.PRIMARY_BLUE,
+              marginTop: '0.5px'
+            }}
+          />
+        </Tooltip>
       ) : role === Role.WORKING_HOLIDAY ? (
-        <LocalAirportIcon
-          style={{
-            fontSize: '17px',
-            color: COLOURS.PRIMARY_BLUE,
-            marginTop: '0.5px'
-          }}
-        />
+        <Tooltip title="워홀러" placement="bottom" arrow>
+          <LocalAirportIcon
+            style={{
+              fontSize: '17px',
+              color: COLOURS.PRIMARY_BLUE,
+              marginTop: '0.5px'
+            }}
+          />
+        </Tooltip>
       ) : role === Role.BUSINESS ? (
-        <BusinessIcon
-          style={{
-            fontSize: '17px',
-            color: COLOURS.PRIMARY_BLUE,
-            marginTop: '0.5px'
-          }}
-        />
+        <Tooltip title="비즈니스" placement="bottom" arrow>
+          <BusinessIcon
+            style={{
+              fontSize: '17px',
+              color: COLOURS.PRIMARY_BLUE,
+              marginTop: '0.5px'
+            }}
+          />
+        </Tooltip>
       ) : role === Role.FREE_MAN ? (
-        <SkateboardingIcon
-          style={{
-            fontSize: '18px',
-            color: COLOURS.LIGHT_PURPLE,
-            marginTop: '0.5px'
-          }}
-        />
+        <Tooltip title="자유인" placement="bottom" arrow>
+          <SkateboardingIcon
+            style={{
+              fontSize: '18px',
+              color: COLOURS.LIGHT_PURPLE,
+              marginTop: '0.5px'
+            }}
+          />
+        </Tooltip>
       ) : role === Role.MYSTIC ? (
-        <AirIcon
-          style={{
-            fontSize: '18px',
-            color: COLOURS.LIGHT_PURPLE,
-            marginTop: '0.5px'
-          }}
-        />
+        <Tooltip title="신비주의" placement="bottom" arrow>
+          <AirIcon
+            style={{
+              fontSize: '18px',
+              color: COLOURS.LIGHT_PURPLE,
+              marginTop: '0.5px'
+            }}
+          />
+        </Tooltip>
       ) : (
         // handling undefined
         // Role.BASE
-        <AccountBoxIcon
-          style={{
-            fontSize: '17px',
-            color: 'lightslategray',
-            marginTop: '0.5px'
-          }}
-        />
+        <Tooltip title="일반인" placement="bottom" arrow>
+          <AccountBoxIcon
+            style={{
+              fontSize: '17px',
+              color: 'lightslategray',
+              marginTop: '0.5px'
+            }}
+          />
+        </Tooltip>
       )}
       <div style={{ color: COLOURS.SECONDARY_SPACE_GREY, fontSize: '0.85rem' }}>
         {username}
