fcb241c7206403920f4fb25083199d7ffcf5351a
Daniel Park <dpark@littles.co>
2022-06-23 15:57:00 +1000

    temp data inserts

---
+++ pages/playground.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react'
+
+const PlayGround: FC = () => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          return null
+        }}
+      >
+        TEST
+      </button>
+    </div>
+  )
+}
+
+export default PlayGround
